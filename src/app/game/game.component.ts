13900da1865f02c477cac078226a2c1e7b096c27
NemanjaDj <nemanjadjeric94@gmail.com>
2023-02-13 13:49:46 +0100

    reroll and continue logic added

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -7,7 +7,7 @@ import { GameComponentServiceService } from './game.component.service.service';
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css']
+  styleUrls: ['./game.component.css',]
 })
 export class GameComponent implements OnInit{
   hand: Hand;
@@ -17,11 +17,14 @@ export class GameComponent implements OnInit{
   handPower = HandPowerType[HandPowerType.FiveOfAKind];
   handNumbers: number[] = [];
   isFirstThrow = true;
+  canReroll = true;
   
   constructor(private gameService: GameComponentServiceService) { }
 
   ngOnInit() {
     this.hand = {id: 1, numbers: '12345', numberOfThrows: 2};
+    this.isFirstThrow = true;
+    this.canReroll = true;
   }
 
   getHand(){
@@ -47,6 +50,7 @@ export class GameComponent implements OnInit{
         this.handPower = HandPowerType[a.handPowerType]
       }),
       this.getNumbers();
+      this.isFirstThrow = false;
     })
   }
 
@@ -62,6 +66,27 @@ export class GameComponent implements OnInit{
   }
 
   reroll(){
-    
+    this.gameService.rerollDices(this.hand.id,[1, 2]).pipe(take(1)).subscribe(data =>{
+      this.hand.numbers = this.trimNumbers(data.numbers),
+      this.hand.numberOfThrows = data.numberOfThrows,
+      console.log(data),
+      console.log(data.id),
+      this.gameService.getHandPower(data.id).pipe(take(1)).subscribe(a => {
+        this.handPower = HandPowerType[a.handPowerType]
+      }),
+      this.getNumbers();
+      this.checkIfRerollIsPossible();
+    });
+  }
+
+  checkIfRerollIsPossible(){
+    if(this.hand.numberOfThrows >= 3){
+      this.canReroll = false;
+    }
+  }
+
+  setNewRound(){
+    this.isFirstThrow = true;
+    this.canReroll = true;
   }
 }
